d648aa26d2a687e43cb46f2aa239d5c5f6368bf4
mohammadsalek <salek.mohmd@gmail.com>
2020-07-21 10:30:58 +0430

    init /database and test

---
+++ nebixbot/database/driver.py
@@ -0,0 +1,25 @@
+import redis
+import logging
+
+
+# class Redis:
+#     """Redis database class"""
+#
+#     def __init__(self):
+#         self.redis_host = "localhost"
+#         self.redis_port = 6379
+#         self.redis_password = ""
+#
+#     def connect(self):
+#         """Return redis object"""
+#         try:
+#             r = redis.Redis(
+#                 host=self.redis_host,
+#                 port=self.redis_port,
+#                 password=self.redis_password,
+#                 decode_responses=True
+#             )
+#             return r
+#         except Exception as err:
+#             print(err)
+#             return None
