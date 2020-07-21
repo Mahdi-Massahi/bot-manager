f2af58afcff4cc0529bfd1ff9c069f6f500f1445
mohammadsalek <salek.mohmd@gmail.com>
2020-07-21 10:37:11 +0430

    add base_strategy tests

---
+++ nebixbot/test/command_center/test_base_strategy.py
@@ -0,0 +1,32 @@
+import unittest
+
+from nebixbot.log.logger import delete_log_file
+from nebixbot.command_center.strategy.base_strategy import BaseStrategy
+from nebixbot.command_center.strategy.sample_strategy.main import (
+    SampleStrategy,
+)
+
+
+class TestBaseStrategy(unittest.TestCase):
+    """Tests for BaseStrategy class"""
+
+    def test_direct_implement(self):
+        """Test not to be able to use BaseStrategy directly"""
+        with self.assertRaises(TypeError) as context:
+            BaseStrategy()
+        self.assertIn(
+            "Can't instantiate abstract class", str(context.exception)
+        )
+
+    def test_implement_abstract_methods(self):
+        """Test abstract methods implementation"""
+        strategy = SampleStrategy("test strategy", "1.0")
+
+        self.assertEqual("test strategy", strategy.name)
+        self.assertEqual("1.0", strategy.version)
+        self.assertEqual("before start", strategy.before_start())
+        self.assertEqual("start", strategy.start())
+        self.assertEqual("before termination", strategy.before_termination())
+
+    def tearDown(self):
+        delete_log_file("test strategy")
