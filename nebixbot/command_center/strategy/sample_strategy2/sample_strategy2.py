1f8f94fb70e9936d539909c1fd99eb8fab5d0107
mohammadsalek <salek.mohmd@gmail.com>
2020-07-23 23:58:41 +0430

    update sample strategy 2

--- nebixbot/command_center/strategy/sample_strategy2/sample_strategy2.py
+++ nebixbot/command_center/strategy/sample_strategy2/sample_strategy2.py
@@ -1,3 +1,6 @@
+import sys
+import time
+
 from nebixbot.command_center.strategy.base_strategy import BaseStrategy
 
 
@@ -10,19 +13,25 @@ class SampleStrategy2(BaseStrategy):
 
     def before_start(self):
         """Strategy Manager calls this before running the strategy"""
-        self.logger.info("it's before start")
+        self.logger.info("BEFORE START")
 
     def start(self):
         """This method is called when algorithm is run"""
-        self.logger.info("it's start")
+        self.logger.info("START")
 
     def before_termination(self):
         """Strategy Manager calls this before terminating a running strategy"""
-        self.logger.info("it's before termination")
+        self.logger.info("BEFORE TERMINATION")
 
 
 if __name__ == '__main__':
-    strategy = SampleStrategy2('sample strategy 2', '0.0.1')
-    strategy.before_start()
-    strategy.start()
-    strategy.before_termination()
+    strategy = SampleStrategy2('Sample Strategy 2', '0.0.1')
+    try:
+        strategy.before_start()
+        strategy.start()
+        strategy.before_termination()
+    except Exception as err:
+        strategy.logger.error(err)
+    finally:
+        strategy.logger.info('Exiting Now...')
+        sys.exit()
