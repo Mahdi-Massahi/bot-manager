7282bc462d284f2d4fd31cf08b9b112c3bdc0293
mohammadsalek <salek.mohmd@gmail.com>
2020-07-29 08:08:35 +0430

    update setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 from setuptools import setup
 
 
-LONG_DESC = "Nebix Trading Bot"
+LONG_DESC = """This is Nebix Trading Bot"""
 
 setup(
     name="nebixbot",
-    version="0.1.0",
+    version="0.9.0",
     description="Nebix Trading Bot",
     long_description=LONG_DESC,
     long_description_content_type="text/plain",
@@ -14,7 +14,7 @@ setup(
     url=" ",
     license="MIT",
     include_package_data=True,
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     packages=setuptools.find_packages(),
     entry_points={"console_scripts": ["nebixbot = nebixbot.__main__:main"]},
 )
