086b613ce615cb38700fea8609d4181427e44e0d
mohammadsalek <salek.mohmd@gmail.com>
2020-08-12 16:09:13 +0430

    remove base client

--- nebixbot/api_client/base_client.py
+++
@@ -1,7 +0,0 @@
-class BaseClient:
-    """Gives the common functionality for inherited clients"""
-
-    def __init__(self, name, version):
-        """Initializes BaseClient"""
-        self.name = name
-        self.version = version
