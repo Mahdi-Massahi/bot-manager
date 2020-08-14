9b0e34c04868907fe1f5f9fedabe014e9564a039
mohammadsalek <salek.mohmd@gmail.com>
2020-08-14 19:30:43 +0430

    update kline

--- nebixbot/command_center/strategy/kline_strategy/kline_strategy.py
+++ nebixbot/command_center/strategy/kline_strategy/kline_strategy.py
@@ -32,69 +32,74 @@ class NebStrategy(BaseStrategy):
 
         symbol = Symbol.BTCUSD
         limit = 200
-        for interval in Interval.values():
-            if interval == Interval.Y:
-                continue
-            (
-                next_kline_ts,
-                last_kline_ts,
-                delta,
-            ) = self.client.get_kline_open_timestamps(symbol, interval)
-            from_ts = next_kline_ts - delta * limit
-            from_ts = 0 if from_ts < 0 else from_ts
-            from_dt = timestamp_to_datetime(from_ts)
-
-            res = self.client.get_kline(symbol, interval, from_dt, limit)
-
-            # if results exits in response:
-            if res and "result" in res and res["result"]:
-                self.logger.info(
-                    f"Writing kline csv results for symbol:{symbol}, " +
-                    f"interval:{interval}..."
-                )
-                results = [
-                    [
-                        "Index",
-                        "Open",
-                        "Close",
-                        "High",
-                        "Low",
-                        "Volume",
-                        "TimeStamp",
-                    ]
-                ]
-                for count, kline in enumerate(res["result"]):
-                    results.append(
-                        [
-                            count + 1,
-                            kline["open"],
-                            kline["close"],
-                            kline["high"],
-                            kline["low"],
-                            kline["volume"],
-                            kline["open_time"],
-                        ]
-                    )
-
-                filepath = NebStrategy.get_filepath(
-                    f"kline_{symbol}_{interval}.csv"
-                )
-                with open(filepath, "w+", newline="") as csv_file:
-                    writer = csv.writer(csv_file)
-                    writer.writerows(results)
-                    self.logger.info("Successfully wrote results to file")
-
-            else:
-                self.logger.error(
-                    "Something was wrong with API response. "
-                    + f"The response was: {res}"
-                )
+        interval = Interval.i5
+        filepath = NebStrategy.get_filepath(
+            f"kline_{symbol}_{interval}.csv"
+        )
+        self.get_kline_data(symbol, limit, interval, filepath)
 
     def before_termination(self, *args, **kwargs):
         """Strategy Manager calls this before terminating a running strategy"""
         self.logger.info("inside before_termination()")
         # Do not delete this line:
         super().before_termination()
+
+    def get_kline_data(self, symbol, limit, interval, filepath):
+        """Get kline data"""
+        if interval == Interval.Y:
+            return
+        (
+            next_kline_ts,
+            last_kline_ts,
+            delta,
+        ) = self.client.get_kline_open_timestamps(symbol, interval)
+        from_ts = next_kline_ts - delta * limit
+        from_ts = 0 if from_ts < 0 else from_ts
+        from_dt = timestamp_to_datetime(from_ts)
+
+        res = self.client.get_kline(symbol, interval, from_dt, limit)
+
+        # if results exits in response:
+        if res and "result" in res and res["result"]:
+            self.logger.info(
+                f"Writing kline csv results for symbol:{symbol}, " +
+                f"interval:{interval}..."
+            )
+            results = [
+                [
+                    "Index",
+                    "Open",
+                    "Close",
+                    "High",
+                    "Low",
+                    "Volume",
+                    "TimeStamp",
+                ]
+            ]
+            for count, kline in enumerate(res["result"]):
+                results.append(
+                    [
+                        count + 1,
+                        kline["open"],
+                        kline["close"],
+                        kline["high"],
+                        kline["low"],
+                        kline["volume"],
+                        kline["open_time"],
+                    ]
+                )
+
+
+            with open(filepath, "w+", newline="") as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerows(results)
+                self.logger.info("Successfully wrote results to file")
+
+        else:
+            self.logger.error(
+                "Something was wrong with API response. "
+                + f"The response was: {res}"
+            )
 
     @staticmethod
     def get_filepath(filename):
