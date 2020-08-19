a83ecb2531296892dca530f413fdce7c27f729cb
mohammadsalek <salek.mohmd@gmail.com>
2020-08-19 17:11:46 +0430

    bug fix todo

--- nebixbm/database/driver.py
+++ nebixbm/database/driver.py
@@ -20,7 +20,7 @@ class RedisDB:
 
     def create_redis_obj(self):
         """Return redis object"""
-        try:  # Singleton 
+        try:  # TODO: Singleton
             redis_obj = redis.Redis(
                 host=self.redis_host,
                 port=self.redis_port,
