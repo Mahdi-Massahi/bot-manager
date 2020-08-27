e8d34301749146946902856720e4582d91f09a17
mohammadsalek <salek.mohmd@gmail.com>
2020-08-27 16:12:34 +0430

    added more tests to validator

--- nebixbm/test/command_center/test_tools.py
+++ nebixbm/test/command_center/test_tools.py
@@ -2,7 +2,10 @@ import csv
 import unittest
 import os
 
-from nebixbm.command_center.tools.csv_validator import csv_kline_validator
+from nebixbm.command_center.tools.csv_validator import (
+    csv_kline_validator,
+    validate_two_csvfiles,
+)
 
 
 def delete_file(filename):
@@ -27,7 +30,8 @@ class TestValidator(unittest.TestCase):
     def setUp(self):
         """Sets up for each test"""
         self.csvfile = "tempfile.csv"
-        with open(self.csvfile, "w"):
+        self.second_csvfile = "second_csvfile.csv"
+        with open(self.csvfile, "w"), open(self.second_csvfile, "w"):
             pass
 
     def test_first_line(self):
@@ -132,6 +136,109 @@ class TestValidator(unittest.TestCase):
 
         self.assertFalse(res)
 
+    def test_two_correct_csv(self):
+        """Test index of two csv files are the same in each row"""
+        append_to_csvfile(
+            self.csvfile,
+            ["Index", "Open", "Close", "High", "Low", "Volume", "TimeStamp"]
+        )
+        append_to_csvfile(
+            self.csvfile,
+            ["1", "100", "100", "100", "100", "100", "100"]
+        )
+        append_to_csvfile(
+            self.csvfile,
+            ["2", "100", "100", "100", "100", "100", "120"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["Index", "Open", "Close", "High", "Low", "Volume", "TimeStamp"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["1", "100", "100", "100", "100", "100", "100"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["2", "100", "100", "100", "100", "100", "120"]
+        )
+        res1, _ = csv_kline_validator(self.csvfile)
+        res2, _ = csv_kline_validator(self.second_csvfile)
+        res3, _ = validate_two_csvfiles(self.csvfile, self.second_csvfile)
+
+        self.assertTrue(res1)
+        self.assertTrue(res2)
+        self.assertTrue(res3)
+
+    def test_two_incorrect_csv_index(self):
+        """Test index of two csv files are the same in each row"""
+        append_to_csvfile(
+            self.csvfile,
+            ["Index", "Open", "Close", "High", "Low", "Volume", "TimeStamp"]
+        )
+        append_to_csvfile(
+            self.csvfile,
+            ["1", "100", "100", "100", "100", "100", "100"]
+        )
+        append_to_csvfile(
+            self.csvfile,
+            ["2", "100", "100", "100", "100", "100", "120"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["Index", "Open", "Close", "High", "Low", "Volume", "TimeStamp"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["1", "100", "100", "100", "100", "100", "100"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["3", "100", "100", "100", "100", "100", "120"]
+        )
+        res1, _ = csv_kline_validator(self.csvfile)
+        res2, _ = csv_kline_validator(self.second_csvfile)
+        res3, _ = validate_two_csvfiles(self.csvfile, self.second_csvfile)
+
+        self.assertTrue(res1)
+        self.assertFalse(res2)
+        self.assertFalse(res3)
+
+    def test_two_incorrect_csv_timestamps(self):
+        """Test index of two csv files are the same in each row"""
+        append_to_csvfile(
+            self.csvfile,
+            ["Index", "Open", "Close", "High", "Low", "Volume", "TimeStamp"]
+        )
+        append_to_csvfile(
+            self.csvfile,
+            ["1", "100", "100", "100", "100", "100", "100"]
+        )
+        append_to_csvfile(
+            self.csvfile,
+            ["2", "100", "100", "100", "100", "100", "120"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["Index", "Open", "Close", "High", "Low", "Volume", "TimeStamp"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["1", "100", "100", "100", "100", "100", "100"]
+        )
+        append_to_csvfile(
+            self.second_csvfile,
+            ["2", "100", "100", "100", "100", "100", "190"]
+        )
+        res1, _ = csv_kline_validator(self.csvfile)
+        res2, _ = csv_kline_validator(self.second_csvfile)
+        res3, _ = validate_two_csvfiles(self.csvfile, self.second_csvfile)
+
+        self.assertTrue(res1)
+        self.assertTrue(res2)
+        self.assertFalse(res3)
+
     def tearDown(self):
         """Tears down after each test"""
         self.assertTrue(delete_file(self.csvfile))
+        self.assertTrue(delete_file(self.second_csvfile))
