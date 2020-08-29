d67827a60f25f742c8a87efdb6e0b7add94a2307
mohammadsalek <salek.mohmd@gmail.com>
2020-08-29 11:58:56 +0430

    fixed bug

--- nebixbm/database/wait_for_db.py
+++ nebixbm/database/wait_for_db.py
@@ -1,13 +1,14 @@
-import redis
+import sys
 import time
+import redis
 
 
-def is_redis_up():
+def is_redis_up(host, port, password):
     try:
         redis_obj = redis.Redis(
-            host="localhost",
-            port=6379,
-            password=None,
+            host=host,
+            port=port,
+            password=password,
             db=0,
             decode_responses=True,
         )
@@ -20,9 +21,14 @@ def is_redis_up():
         return False
 
 
-while True:
-    print("Trying to connect to Redis...")
-    if is_redis_up():
-        print("Successfully connected to Redis")
-        break
-    time.sleep(1)
+if __name__ == '__main__':
+    arguments = sys.argv
+    host = arguments[0]
+    port = arguments[1]
+    password = arguments[3] if len(arguments) > 2 else None
+    while True:
+        print("Trying to connect to Redis...")
+        if is_redis_up(host, port, password):
+            print("Successfully connected to Redis")
+            break
+        time.sleep(1)
