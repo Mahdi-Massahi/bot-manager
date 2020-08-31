ecf4be7ae4061e373d05e1baa8a9071948167893
mohammadsalek <salek.mohmd@gmail.com>
2020-08-31 17:33:02 +0430

    initialized file

---
+++ nebixbm/__init__.py
@@ -0,0 +1,7 @@
+# You found an easter egg!
+#
+# This project is created with so much love and patience :)
+# And tbh it somehow feels like a child to me haha
+# I hope this project works for ages and doesn't break any time soon!
+#
+# M0hammad Sa1ek - 2020/8/31, 17:30
