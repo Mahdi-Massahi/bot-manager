f89da6c732a0489ece1a3b129fb201c2f94410d8
Mahdi-Massahi <Mahdi.massahi@gmail.com>
2020-09-02 22:58:46 +0430

    added some to do

--- nebixbm/command_center/tools/csv_validator.py
+++ nebixbm/command_center/tools/csv_validator.py
@@ -43,6 +43,8 @@ def csv_kline_validator(csvfile):
         3- TimeStamp must increase by a fixed delta
         4- All values must be bigger than zero
         5- Must not be empty (more than 1 rows)
+        6- TODO: number of rows must be same as the one in request
+        7- TODO: last kline timestamp must be in time between start and next schedule time
     """
     try:
         with open(csvfile, "r", newline="") as csv_file:
