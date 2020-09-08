90d9fad68841a287f0b84532624dcd233713aaf1
mohammadsalek <salek.mohmd@gmail.com>
2020-09-08 21:37:13 +0430

    fixed style

--- nebixbm/log/logger.py
+++ nebixbm/log/logger.py
@@ -52,12 +52,12 @@ def create_logger(name, filename):
             + " (%(filename)s:%(lineno)s)",
             datefmt="%Y-%m-%d %H:%M:%S",
             log_colors={
-        		'DEBUG':    'cyan',
-        		'INFO':     'green',
-        		'WARNING':  'yellow',
-        		'ERROR':    'red',
-        		'CRITICAL': 'red,bg_white',
-        	},
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red,bg_white",
+            },
         )
 
         fh.setFormatter(formatter)
