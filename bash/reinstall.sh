6bc82fd87e48dd5c35717f843171ea881a8c4f1f
mohammadsalek <salek.mohmd@gmail.com>
2020-09-09 13:07:11 +0430

    shows version

--- bash/reinstall.sh
+++ bash/reinstall.sh
@@ -2,4 +2,5 @@
 
 # nebixbm -ta &&
 pip3 install -r requirements.txt &&
-pip3 install --upgrade --force-reinstall .
+pip3 install --upgrade --force-reinstall . &&
+python3 /usr/local/lib/python3.8/site-packages/nebixbm/__main__.py -v
--- nebixbm/__main__.py
+++ nebixbm/__main__.py
@@ -255,10 +255,6 @@ def main():
                     print(f"Error code: {proc.returncode}, message: {errors}")
                 else:
                     print(
-                        "Current version:",
-                        pkg_resources.require("nebixbm")[0].version
-                    )
-                    print(
                         Tcolors.OKGREEN + "Successfully updated" +
                         Tcolors.ENDC
                     )
