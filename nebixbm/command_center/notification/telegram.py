1505ce886a5abd87608b6d0f0c009ebad0b550e7
Mahdi-Massahi <Mahdi.massahi@gmail.com>
2020-09-28 15:42:17 +0330

    revert logger for telegram

--- nebixbm/command_center/notification/telegram.py
+++ nebixbm/command_center/notification/telegram.py
@@ -8,14 +8,14 @@ from nebixbm.log.logger import create_logger, get_file_name
 class TelegramClient:
     def __init__(self):
         filename = get_file_name("EmailSender", None)
-        # self.logger, self.log_filepath = create_logger(filename, filename)
+        self.logger, self.log_filepath = create_logger(filename, filename)
         self.BASE = 'https://api.telegram.org/bot'
         self.USER_ID = os.getenv("NOTIFY_TELEGRAM_ID")
         self.TOKEN = os.getenv("NOTIFY_TELEGRAM_TOKEN")
         self.send_message("Hello from ***NEBIX***.\n\n"
                           "Telegram bot initialized.\n"
                           "Stating trading algo.")
-        # self.logger.info("Successfully initialized telegram bot.")
+        self.logger.info("Successfully initialized telegram bot.")
 
     def send_message(self, message):
         """Sends a message to an specific user"""
@@ -23,14 +23,14 @@ class TelegramClient:
                                        "&parse_mode=Markdown&text=" \
                                        f"{message}" \
                                        f"\n\n``` {str(datetime.today())} ```"
-        # self.logger.info("Successfully sent telegram notification.")
+        self.logger.info("Successfully sent telegram notification.")
         try:
-            # self.logger.debug(str(req))
+            self.logger.debug(str(req))
             res = requests.get(req)
-            # self.logger.debug(str(res))
+            self.logger.debug(str(res))
         except Exception as ex:
             pass
-            # self.logger.error("Error sending telegram notification:", ex)
+            self.logger.error("Error sending telegram notification:", ex)
 
     # def update(self):
     #     """Gets the recent unread chats"""
