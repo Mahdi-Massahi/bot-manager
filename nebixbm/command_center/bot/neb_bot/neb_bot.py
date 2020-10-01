9a8adcfda8546a12ea05279940b0080032bfaa87
Mahdi-Massahi <Mahdi.massahi@gmail.com>
2020-10-01 11:09:48 +0330

    debug

--- nebixbm/command_center/bot/neb_bot/neb_bot.py
+++ nebixbm/command_center/bot/neb_bot/neb_bot.py
@@ -139,7 +139,7 @@ class NebBot(BaseBot):
         self.logger.info("[state-no:2.01]")
 
         # Bot starting datetime
-        start_dt = datetime.datetime(2020, 10, 1, 7, 35, 0)
+        start_dt = datetime.datetime(2020, 10, 1, 7, 45, 0)
         start_ts = datetime_to_timestamp(start_dt, is_utc=True)
 
         # start_ts = timestamp_now() + 50
@@ -1316,7 +1316,7 @@ if __name__ == "__main__":
     try:
         # Change name and version of your bot:
         name = "Neb Bot"
-        version = "0.5.7"
+        version = "0.5.8"
 
         # Do not delete these lines:
         bot = NebBot(name, version)
