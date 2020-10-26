d335888bc89a3c21bb58f56facfd2111866c5e82
Mahdi-Massahi <Mahdi.massahi@gmail.com>
2020-10-26 10:18:40 +0330

    debugging

--- nebixbm/command_center/bot/neb_bot/neb_bot.py
+++ nebixbm/command_center/bot/neb_bot/neb_bot.py
@@ -149,7 +149,7 @@ class NebBot(BaseBot):
         self.logger.info("[state-no:2.01]")
 
         # Bot starting datetime
-        start_dt = datetime.datetime(2020, 10, 26, 6, 43, 0)
+        start_dt = datetime.datetime(2020, 10, 26, 6, 52, 0)
         start_ts = datetime_to_timestamp(start_dt, is_utc=True)
 
         # start_ts = timestamp_now() + 50
