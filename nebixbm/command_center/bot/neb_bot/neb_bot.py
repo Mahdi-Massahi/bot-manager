30ed61c8ba68f8952472c76117ac18d6a7f28dbe
Mahdi Massahi <mahdi.massahi@gmail.com>
2020-11-21 23:56:43 +0330

    debugging

--- nebixbm/command_center/bot/neb_bot/neb_bot.py
+++ nebixbm/command_center/bot/neb_bot/neb_bot.py
@@ -43,7 +43,7 @@ from nebixbm.log.logger import (
 )
 # Change name and version of your bot:
 name = "Neb Bot"
-version = "0.5.15"
+version = "0.5.16"
 
 # save a list of running R subprocesses:
 _r_subp_pid_list = []
@@ -151,13 +151,13 @@ class NebBot(BaseBot):
         self.logger.info("[state-no:2.01]")
 
         # Bot starting datetime
-        start_dt = datetime.datetime(2020, 10, 26, 7, 0, 0)
+        start_dt = datetime.datetime(2020, 11, 21, 20, 50, 0)
         start_ts = datetime_to_timestamp(start_dt, is_utc=True)
 
         # start_ts = timestamp_now() + 50
 
         # Bot termination datetime (end)
-        end_dt = datetime.datetime(2020, 11, 14, 23, 59, 59)
+        end_dt = datetime.datetime(2020, 11, 30, 23, 59, 59)
         end_ts = datetime_to_timestamp(end_dt, is_utc=True)
 
         # first job timestamp (current job):
