a066fa96d666bace5cbf6ec5e04bd57a12a92f76
Mahdi-Massahi <Mahdi.massahi@gmail.com>
2020-11-22 12:07:15 +0330

    updates for notifier debugging

--- nebixbm/command_center/bot/neb_bot/neb_bot.py
+++ nebixbm/command_center/bot/neb_bot/neb_bot.py
@@ -151,7 +151,7 @@ class NebBot(BaseBot):
         self.logger.info("[state-no:2.01]")
 
         # Bot starting datetime
-        start_dt = datetime.datetime(2020, 11, 22, 8, 20, 0)
+        start_dt = datetime.datetime(2020, 11, 22, 8, 40, 0)
         start_ts = datetime_to_timestamp(start_dt, is_utc=True)
 
         # start_ts = timestamp_now() + 50
